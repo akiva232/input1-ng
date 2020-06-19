49f774877223e2a5d6dcde20561cc83598412f0f
akiva <akiva232@gmail.com>
2020-06-19 08:55:28 +0300

    output

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { IOFNComponent } from './c/io-fn/io-fn.component';
 import { ParentComponent } from './c/IO-FN/parent/parent.component';
 import { ChildComponent } from './c/IO-FN/child/child.component';
+import { ChildOutputComponent } from './c/IO-FN/child-output/child-output.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IOFNComponent,
     ParentComponent,
-    ChildComponent
+    ChildComponent,
+    ChildOutputComponent
   ],
   imports: [
     BrowserModule
